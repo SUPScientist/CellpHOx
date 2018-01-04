67a106036cc0d47762c1c208cc0b88680ea6a5f6
PJB <pjbresnahan@gmail.com>
2018-01-03 17:39:28 -0800

    changed wait timing to remove hard-coded delays

--- src/Basic_Serial_Poll.ino
+++ src/Basic_Serial_Poll.ino
@@ -5,8 +5,10 @@
  * Date: 2018-01-03
  */
 
-// Turn cell modem off for now
-SYSTEM_MODE(MANUAL);
+SYSTEM_MODE(MANUAL); // Turn cell modem off for now
+const unsigned long SEND_INTERVAL_MS = 30000; // How often we poll SeapHOx in millis
+unsigned long lastSend = 0; // Use for wait timing
+
 
 void setup() {
 	Cellular.off();
@@ -27,14 +29,17 @@ void setup() {
 
 // Give SeapHOx arbitrary wake character then "ts" command. Then listen for response.
 void loop() {
-  Serial1.print("a");         // arbitrary
-  delay(500);                   // let SeapHOx wake up
-  Serial1.println("ts");        // take sample
-  delay(500);                   // wait for response
+	// When we hit timing interval, send wake then "ts" and listen
+	if (millis() - lastSend >= SEND_INTERVAL_MS) {
+		lastSend = millis();
+	  Serial1.print("a");         // arbitrary
+	  delay(500);                   // let SeapHOx wake up
+	  Serial1.println("ts");        // take sample
+	  delay(500);                   // wait for response (~ 10 seconds)
 
-	String s = Serial1.readString();
-	Serial.printlnf("got %s", s.c_str());
-
-  delay(60000);                 // repeat in a minute; TODO: change to be a clean minute
-	Serial.println("REPEAT");
+		String s = Serial1.readString();
+		Serial.printlnf("got %s", s.c_str());
+		Serial.println("REPEAT");
+	}
+	delay(100); // in case it matters that otherwise it'll just spin at clock speed
 }
