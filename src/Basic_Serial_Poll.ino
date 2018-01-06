f6069bff103c763c0c2d3f80cad69997ffb2dd7e
PJB <pjbresnahan@gmail.com>
2018-01-05 16:29:32 -0800

    soldered MAX3232 in between SeapHOx and Elec and added DIO switch

--- src/Basic_Serial_Poll.ino
+++ src/Basic_Serial_Poll.ino
@@ -5,6 +5,8 @@
  * Date: 2018-01-03
  */
 
+
+int max3232_switch = A1; // DIO to turn on/off MAX3232 chip
 SYSTEM_MODE(MANUAL); // Turn cell modem off for now
 const unsigned long SEND_INTERVAL_MS = 30000; // How often we poll SeapHOx in millis
 unsigned long lastSend = 0; // Use for wait timing
@@ -13,14 +15,18 @@ char* new_var;
 void setup() {
 	Cellular.off();
 
+	// Sets switch pin to output and defaults to off
+	pinMode(max3232_switch, OUTPUT);
+	digitalWrite(max3232_switch, LOW);
+
   // To print to screen over USB-Serial
 	Serial.begin(19200);
 
   // To communicate with peripheral (SeapHOx is peripheral to Particle)
 	Serial1.begin(115200);
 
-	// Wait up to 15 seconds for a line to arrive
-	Serial1.setTimeout(15000);
+	// Wait for a line to arrive, max this out for now
+	Serial1.setTimeout(SEND_INTERVAL_MS);
 
   // Wait a few seconds then let us know program is running
   delay(5000);
@@ -31,15 +37,18 @@ void setup() {
 void loop() {
 	// When we hit timing interval, send wake then "ts" and listen
 	if (millis() - lastSend >= SEND_INTERVAL_MS) {
-		lastSend = millis();
-	  Serial1.print("a");         // arbitrary
+		lastSend = millis(); // don't put anything above this to keep loop timing tight
+
+		digitalWrite(max3232_switch, HIGH); // turn on MAX3232
+		// delay(1500);									// this is superstition; I don't know how long that thing needs to "warm up"
+	  Serial1.print("a");         	// arbitrary wake char
 	  delay(500);                   // let SeapHOx wake up
 	  Serial1.println("ts");        // take sample
-	  delay(500);                   // wait for response (~ 10 seconds)
+		Serial.println("Taking sample now...");
 
 		String s = Serial1.readString();
 		const char* s_args = s.c_str();
-		Serial.printlnf("got %s", s.c_str()); // s.c_str() gives args of s
+		// Serial.printlnf("got %s", s.c_str()); // s.c_str() gives args of s
 
 		// Parse out SeapHOx response; TODO: clean up malloc issues from using strdup by using free()
 		new_var = strtok(strdup(s_args), "\t");
@@ -49,6 +58,11 @@ void loop() {
 		}
 
 		Serial.println("REPEAT"); // just for kicks
+
+		// Wait till we've received response before turning of MAX3232
+		// delay(1500);
+		// Serial1.flush();
+		// digitalWrite(max3232_switch, LOW);
   }
 
 	delay(100); // in case it matters that otherwise it'll just spin at clock speed
