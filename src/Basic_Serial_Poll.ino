a3b045b485c9da4aee6db08ec83e09ccc01629e0
PJB <pjbresnahan@gmail.com>
2018-01-09 10:12:34 -0800

    changed lastSend to ensure polling on first loop

--- src/Basic_Serial_Poll.ino
+++ src/Basic_Serial_Poll.ino
@@ -9,8 +9,9 @@
 int counter = 0;
 int max3232_switch = A1; // DIO to turn on/off MAX3232 chip
 SYSTEM_MODE(MANUAL); // Turn cell modem off for now
-const unsigned long SEND_INTERVAL_MS = 30000; // How often we poll SeapHOx in millis
-unsigned long lastSend = 0; // Use for wait timing
+const unsigned long SEND_INTERVAL_MS = 60000; // How often we poll SeapHOx in millis
+const unsigned long TIMEOUT_SEAPHOX_MS = 20000; // Max wait time for SeapHOx response
+unsigned long lastSend = -1*SEND_INTERVAL_MS; // Use for wait timing; starting negative forces it to work on first loop
 char* new_var;
 
 void setup() {
@@ -27,7 +28,7 @@ void setup() {
 	Serial1.begin(115200);
 
 	// Wait for a line to arrive, max this out for now
-	Serial1.setTimeout(20000);
+	Serial1.setTimeout(TIMEOUT_SEAPHOX_MS);
 
   // Wait a few seconds then let us know program is running
   delay(5000);
@@ -36,11 +37,14 @@ void setup() {
 
 // Give SeapHOx arbitrary wake character then "ts" command. Then listen for response.
 void loop() {
+	// Serial.println(millis()); // for debugging only
+
 	// When we hit timing interval, send wake then "ts" and listen
 	if (millis() - lastSend >= SEND_INTERVAL_MS) {
 		lastSend = millis(); // don't put anything above this to keep loop timing tight
 
 		digitalWrite(max3232_switch, HIGH); // turn on MAX3232
+		delay(500);
 	  Serial1.print("a");         	// arbitrary wake char
 	  delay(500);                   // let SeapHOx wake up
 	  Serial1.println("ts");        // take sample
@@ -48,7 +52,7 @@ void loop() {
 
 		String s = Serial1.readString();
 		const char* s_args = s.c_str();
-		// Serial.printlnf("got %s", s.c_str()); // s.c_str() gives args of s
+		Serial.printlnf("got %s", s.c_str()); // s.c_str() gives args of s
 
 		// Parse out SeapHOx response; TODO: clean up malloc issues from using strdup by using free()
 		new_var = strtok(strdup(s_args), "\t");
@@ -57,12 +61,15 @@ void loop() {
 			new_var = strtok(NULL, " \t");
 		}
 
-		// digitalWrite(max3232_switch, LOW); // turn off MAX3232
+		delay(500);
+		digitalWrite(max3232_switch, LOW); // turn off MAX3232
+
 
 		Serial.printlnf("Loop %i; REPEAT\n", counter);
 		counter++;
   }
 
+	// TODO: change delay to deep sleep
 	delay(100); // in case it matters that otherwise it'll just spin at clock speed
 
 }
