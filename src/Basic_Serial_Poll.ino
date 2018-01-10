ee1c662383593576d6ac82bc2e4684e026304901
PJB <pjbresnahan@gmail.com>
2018-01-09 18:38:04 -0800

    close and open serial port every time to allow depowering MAX3232

--- src/Basic_Serial_Poll.ino
+++ src/Basic_Serial_Poll.ino
@@ -11,7 +11,7 @@ int max3232_switch = A1; // DIO to turn on/off MAX3232 chip
 SYSTEM_MODE(MANUAL); // Turn cell modem off for now
 
 const unsigned long SEND_INTERVAL_MS = 30000; // How often we poll SeapHOx in millis
-const unsigned long TIMEOUT_SEAPHOX_MS = 60000; // Max wait time for SeapHOx response
+const unsigned long TIMEOUT_SEAPHOX_MS = 20000; // Max wait time for SeapHOx response
 const unsigned long SLEEP_TIME_SEC = 30; // Deep sleep time
 
 unsigned long lastSend = -1*SEND_INTERVAL_MS; // Use for wait timing; starting negative forces it to work on first loop
@@ -34,45 +34,45 @@ void setup() {
 	// Wait for a line to arrive, max this out for now
 	Serial1.setTimeout(TIMEOUT_SEAPHOX_MS);
 
-  // Wait a few seconds then let us know program is running
-  delay(5000);
-  Serial.println("Beginning to poll");
 }
 
 // Give SeapHOx arbitrary wake character then "ts" command. Then listen for response.
 void loop() {
 
-	digitalWrite(max3232_switch, HIGH); // turn on MAX3232
-	delay(500);
-  Serial1.print("a");         	// arbitrary wake char
-  delay(500);                   // let SeapHOx wake up
-  Serial1.println("ts");        // take sample
-	Serial1.flush();
+	if(millis()-lastSend >= SEND_INTERVAL_MS){
+		lastSend = millis();
 
-	Serial.println("Taking sample now...");
-	Serial.flush();
+		digitalWrite(max3232_switch, HIGH); // turn on MAX3232
+		Serial1.end();	// clean out any residual junk in buffer
+		delay(500);
+		Serial1.begin(115200);
 
-	Serial.println(Serial1.readString());
-	Serial.flush();
+		// delay(500);
+		// Serial1.print("a");         	// arbitrary wake char
+		delay(500);
+	  Serial1.println("ts");        // take sample
 
-	Serial.printlnf("Loop %i; REPEAT\n", counter);
-	Serial.flush();
-	counter++;
+		// Parse out SeapHOx response; TODO: clean up malloc issues from using strdup by using free()
+		s = Serial1.readString();			// read response
+		const char* s_args = s.c_str();
+		new_var = strtok(strdup(s_args), "\t");
+		while (new_var != NULL) {
+			Serial.println(new_var);
+			new_var = strtok(NULL, " \t");
+		}
+		// delete s_args; // hopefully prevent memory leakage, also SOS
+		// free((char*)s_args); // caused Elec to go into SOS mode!
+		// free( (void*)(s_args) );
 
+		delay(500);
+
+		counter++;
+	}
+
+	digitalWrite(max3232_switch, LOW);
 	// Turn off microcontroller and cellular.
 	// Reset after seconds.
 	// Ultra low power usage.
-	System.sleep(SLEEP_MODE_DEEP, SLEEP_TIME_SEC);
+	// System.sleep(SLEEP_MODE_DEEP, SLEEP_TIME_SEC);
 
 }
-
-
-// s = Serial1.readString();			// read response
-//
-// // Parse out SeapHOx response; TODO: clean up malloc issues from using strdup by using free()
-// const char* s_args = s.c_str();
-// new_var = strtok(strdup(s_args), "\t");
-// while (new_var != NULL) {
-// 	Serial.println(new_var);
-// 	new_var = strtok(NULL, " \t");
-// }
