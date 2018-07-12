e0e24e7169d3f875096909bc25d7906f7631e1ca
pjb-mbp15 <pjbresnahan@gmail.com>
2018-07-12 15:39:04 -0700

    Remove unneeded constants and switch from SYSTEM_MODE(MANUAL) to (SEMI_AUTOMATIC)

--- GPSandCell_test/src/GPSandCell_test.ino
+++ GPSandCell_test/src/GPSandCell_test.ino
@@ -1,4 +1,3 @@
-
 #include "Particle.h"
 #include "TinyGPS++.h"
 
@@ -11,33 +10,29 @@
 
 // System threading is required for this project
 SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(AUTOMATIC);
+SYSTEM_MODE(SEMI_AUTOMATIC);
 
 // Global objects
 FuelGauge batteryMonitor;
-
 TinyGPSPlus gps;
 
-
+// Forward declaration
 void displayInfo();
 
 // This is the name of the Particle event to publish for battery or movement detection events
 // It is a private event.
-const char *eventName = "accel";
+const char *eventName = "pHOx";
 
 // Various timing constants
 const unsigned long MAX_TIME_TO_PUBLISH_MS = 60000; // Only stay awake for 60 seconds trying to connect to the cloud and publish
 const unsigned long MAX_TIME_FOR_GPS_FIX_MS = 120000; // Only stay awake for 3 minutes trying to get a GPS fix
 const unsigned long TIME_AFTER_PUBLISH_MS = 4000; // After publish, wait 4 seconds for data to go out
 const unsigned long TIME_AFTER_BOOT_MS = 5000; // At boot, wait 5 seconds before going to sleep again (after coming online)
-const unsigned long TIME_PUBLISH_BATTERY_SEC = 22 * 60; // every 22 minutes send a battery update to keep the cellular connection up
-const unsigned long SERIAL_PERIOD = 5000;
-const unsigned long MAX_GPS_AGE_MS = 10000; // GPS location must be newer than this to be considered valid
-
+const unsigned long SERIAL_PERIOD_MS = 5000;
 
 // Stuff for the finite state machine
-enum State { ONLINE_WAIT_STATE, RESET_STATE, RESET_WAIT_STATE, PUBLISH_STATE, SLEEP_STATE, SLEEP_WAIT_STATE, BOOT_WAIT_STATE, GPS_WAIT_STATE };
-State state = ONLINE_WAIT_STATE;
+enum State { PUBLISH_STATE, SLEEP_STATE, SLEEP_WAIT_STATE, BOOT_WAIT_STATE, GPS_WAIT_STATE };
+State state = GPS_WAIT_STATE;
 
 unsigned long stateTime = 0;
 unsigned long lastSerial = 0;
@@ -53,12 +48,12 @@ void setup() {
   state = GPS_WAIT_STATE;
   Serial.println("turn cell on and wait for GPS fix");
 
-	Serial.begin(9600);
+  Serial.begin(9600);
 
-	// The GPS module on the AssetTracker is connected to Serial1 and D6
-	Serial1.begin(9600);
+  // The GPS module on the AssetTracker is connected to Serial1 and D6
+  Serial1.begin(9600);
 
-	// Settings D6 LOW powers up the GPS module
+  // Settings D6 LOW powers up the GPS module
   pinMode(D6, OUTPUT);
   digitalWrite(D6, LOW);
   startFix = millis();
@@ -67,123 +62,122 @@ void setup() {
 
 
 void loop() {
-	while (Serial1.available() > 0) {
-		if (gps.encode(Serial1.read())) {
-			displayInfo();
-		}
-	}
+  while (Serial1.available() > 0) {
+  if (gps.encode(Serial1.read())) {
+  displayInfo();
+  }
+  }
 
-	switch(state) {
+  switch(state) {
+  case GPS_WAIT_STATE:
 
-	case GPS_WAIT_STATE:
+    Particle.connect();
 
-    Cellular.on();
-
-		if (gps.location.isValid() && gps.location.age() < MAX_GPS_AGE_MS) {
-			// Got a GPS fix
+  if (gps.location.isValid()) {
+  // Got a GPS fix
       latitude = gps.location.lat();
       longitude = gps.location.lng();
-			state = PUBLISH_STATE;
-			break;
-		}
-		if (millis() - stateTime >= MAX_TIME_FOR_GPS_FIX_MS) {
-			Serial.println("failed to get GPS fix");
+  state = PUBLISH_STATE;
+  break;
+  }
+  if (millis() - stateTime >= MAX_TIME_FOR_GPS_FIX_MS) {
+  Serial.println("failed to get GPS fix");
       latitude = 0;
       longitude = 0;
-			state = PUBLISH_STATE;
-			break;
+  state = PUBLISH_STATE;
+  break;
 
-		}
-		break;
+  }
+  break;
 
-	case PUBLISH_STATE:
+  case PUBLISH_STATE:
 
-		if (Particle.connected()) {
-			// The publish data contains 3 comma-separated values:
-			// whether movement was detected (1) or not (0) The not detected publish is used for battery status updates
-			// cell voltage (decimal)
-			// state of charge (decimal)
-			char data[64];
-			float cellVoltage = batteryMonitor.getVCell();
-			float stateOfCharge = batteryMonitor.getSoC();
-			snprintf(data, sizeof(data), "online: %.02f,%.02f,%f,%f",
-					cellVoltage, stateOfCharge, latitude, longitude);
+  if (Particle.connected()) {
+  // The publish data contains 3 comma-separated values:
+  // whether movement was detected (1) or not (0) The not detected publish is used for battery status updates
+  // cell voltage (decimal)
+  // state of charge (decimal)
+  char data[64];
+  float cellVoltage = batteryMonitor.getVCell();
+  float stateOfCharge = batteryMonitor.getSoC();
+  snprintf(data, sizeof(data), "online: %.02f,%.02f,%f,%f",
+  cellVoltage, stateOfCharge, latitude, longitude);
 
-			Particle.publish(eventName, data, 60, PRIVATE);
-			Serial.println(data);
+  Particle.publish(eventName, data, 60, PRIVATE);
+  Serial.println(data);
 
-			// Wait for the publish to go out
-			stateTime = millis();
-			state = SLEEP_WAIT_STATE;
-		}
-		else {
-			// Haven't come online yet
-			if (millis() - stateTime >= MAX_TIME_TO_PUBLISH_MS) {
-				// Took too long to publish, just go to sleep
+  // Wait for the publish to go out
+  stateTime = millis();
+  state = SLEEP_WAIT_STATE;
+  }
+  else {
+  // Haven't come online yet
+  if (millis() - stateTime >= MAX_TIME_TO_PUBLISH_MS) {
+  // Took too long to publish, just go to sleep
         char data[64];
-  			float cellVoltage = batteryMonitor.getVCell();
-  			float stateOfCharge = batteryMonitor.getSoC();
-  			snprintf(data, sizeof(data), "offline: %.02f,%.02f,%f,%f",
-  					cellVoltage, stateOfCharge, latitude, longitude);
+        float cellVoltage = batteryMonitor.getVCell();
+        float stateOfCharge = batteryMonitor.getSoC();
+        snprintf(data, sizeof(data), "offline: %.02f,%.02f,%f,%f",
+            cellVoltage, stateOfCharge, latitude, longitude);
 
-    			Serial.println(data);
+          Serial.println(data);
 
-				state = SLEEP_STATE;
-			}
-		}
-		break;
+  state = SLEEP_STATE;
+  }
+  }
+  break;
 
-	case SLEEP_WAIT_STATE:
-		if (millis() - stateTime >= TIME_AFTER_PUBLISH_MS) {
-			state = SLEEP_STATE;
-		}
-		break;
+  case SLEEP_WAIT_STATE:
+  if (millis() - stateTime >= TIME_AFTER_PUBLISH_MS) {
+  state = SLEEP_STATE;
+  }
+  break;
 
-	case SLEEP_STATE:
+  case SLEEP_STATE:
 
-		// Uncomment this line to power down the GPS. It saves power but may increase the amount
-		// of time to get a fix.
+  // Uncomment this line to power down the GPS. It saves power but may increase the amount
+  // of time to get a fix.
 
     Serial.println("going to sleep");
     delay(500);
 
-  	// Sleep
+    // Sleep
     int secondsToSleep = 180;
 
     // Test delay vs. sleep
     delay(180000);
     stateTime = millis();
-  	// System.sleep(SLEEP_MODE_DEEP, secondsToSleep);
+    // System.sleep(SLEEP_MODE_DEEP, secondsToSleep);
 
     // It'll only make it here if the sleep call doesn't work for some reason
     state = GPS_WAIT_STATE;
-  	break;
+    break;
 
-	}
+  }
 
 }
 
 void displayInfo()
 {
-	if (millis() - lastSerial >= SERIAL_PERIOD) {
-		lastSerial = millis();
+  if (millis() - lastSerial >= SERIAL_PERIOD_MS) {
+  lastSerial = millis();
 
-		char buf[128];
-		if (gps.location.isValid()) {
-			snprintf(buf, sizeof(buf), "%f,%f", gps.location.lat(), gps.location.lng());
-			if (gettingFix) {
-				gettingFix = false;
-				unsigned long elapsed = millis() - startFix;
-				Serial.printlnf("%lu milliseconds to get GPS fix", elapsed);
-			}
-		}
-		else {
-			strcpy(buf, "no location");
-			if (!gettingFix) {
-				gettingFix = true;
-				startFix = millis();
-			}
-		}
-		Serial.println(buf);
-	}
+  char buf[128];
+  if (gps.location.isValid()) {
+  snprintf(buf, sizeof(buf), "%f,%f", gps.location.lat(), gps.location.lng());
+  if (gettingFix) {
+  gettingFix = false;
+  unsigned long elapsed = millis() - startFix;
+  Serial.printlnf("%lu milliseconds to get GPS fix", elapsed);
+  }
+  }
+  else {
+  strcpy(buf, "no location");
+  if (!gettingFix) {
+  gettingFix = true;
+  startFix = millis();
+  }
+  }
+  Serial.println(buf);
+  }
 }
