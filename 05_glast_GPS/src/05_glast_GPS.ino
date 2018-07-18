1e5984f97a36db09ad4766e741611953cfc9617e
pjb-mbp15 <pjbresnahan@gmail.com>
2018-07-18 13:44:17 -0700

    Update 05_glast_GPS to include timing from 04_GData_ReadLoop2 and GPS polling from GPSandCell_test

--- 05_glast_GPS/src/05_glast_GPS.ino
+++ 05_glast_GPS/src/05_glast_GPS.ino
@@ -1,32 +1,34 @@
-/*
- * Project 05_glast_GPS
- * Description:
- * Author: PJB
- * Date: 9-July-2018
- */
-
- //
+#include "Particle.h"
 #include "TinyGPS++.h"
 #include "Serial5/Serial5.h"
-SYSTEM_MODE(MANUAL); // For testing only; COMMENT OUT for deployment
-// SYSTEM_MODE(AUTOMATIC); // For deployment; comment out for testing
-SYSTEM_THREAD(ENABLED);
 
-// Forward declaration
-void parseGPS(); // for GPS polling/parsing
-void publishSeapHOxFake(); // pretend there's a peripheral SeapHOx even if there isn't
-void publishSeapHOx(); // send data string to cloud
+// System threading is required for this project
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+// Global objects
+FuelGauge batteryMonitor;
+TinyGPSPlus gps;
+
+// Forward declarations
+void parseGPS(); //
 void parseSeapHOx(); // parse response from SeapHOx of "ts" "gdata" or "glast" command from Electron
 
-// Set up globals and constants and structs
-const char *eventName = "CpHOx";
-FuelGauge batteryMonitor; // keep an eye on Particle's battery
-const unsigned long TIMEOUT_SEAPHOX_MS = 5000; // Max wait time for SeapHOx response
+// This is the name of the Particle event to publish for battery or movement detection events
+// It is a private event.
+const char *eventName = "CpHOx2";
+
+// Various timing constants
+const unsigned long MAX_TIME_TO_PUBLISH_MS = 60000; // Only stay awake for 60 seconds trying to connect to the cloud and publish
+const unsigned long MAX_TIME_FOR_GPS_FIX_MS = 120000; // Only stay awake for 3 minutes trying to get a GPS fix
+const unsigned long TIME_AFTER_PUBLISH_MS = 4000; // After publish, wait 4 seconds for data to go out
+const unsigned long TIME_AFTER_BOOT_MS = 5000; // At boot, wait 5 seconds before going to sleep again (after coming online)
+const unsigned long SERIAL_PERIOD_MS = 5000; // Interval between GPS serial port checks
 const unsigned long SLEEP_TIME_SEC = 1800; // Deep sleep time
-const unsigned long SERIAL_PERIOD_MS = 180000;
+const unsigned long TIMEOUT_SEAPHOX_MS = 5000; // Max wait time for SeapHOx response
 
+// SeapHOx struct for response variables
 String s;
-
 struct SeapHOx {
   int Sample_Number;     //0
   char *Board_Date;       //1
@@ -45,7 +47,7 @@ struct SeapHOx {
   float Substrate_Leak;   //14
   float Optode_Model;   //15
   float Optode_SN;      //16
-  float O2 ;           //17 yes [µM]
+  float O2uM ;          //17 yes [µM]
   float O2_Saturation ; //18
   float Optode_Temp;    //19
   float Dphase;         //20
@@ -63,81 +65,193 @@ struct SeapHOx {
 };
 SeapHOx SeapHOx_Cell;
 
-// The TinyGPS++ object
-TinyGPSPlus gps;
+// For the finite state machine
+enum State { PUBLISH_STATE, SLEEP_STATE, GPS_WAIT_STATE };
+State state = GPS_WAIT_STATE;
+
+unsigned long stateTime = 0;
 unsigned long lastSerial = 0;
-unsigned long lastPublish = 0;
 unsigned long startFix = 0;
 bool gettingFix = false;
 
-// Start here upon wakeup
+float latitude = 0;
+float longitude = 0;
+
 void setup() {
-  Cellular.off(); // COMMENT FOR REAL TEST
+  // Start here when waking up out of SLEEP_MODE_DEEP
+  state = GPS_WAIT_STATE;
 
-  // To print to screen over USB-Serial
+  // Turn on USB comms
   Serial.begin(9600);
+  Serial.println("turn cell on and wait for GPS fix");
 
-  // SeapHOx
-  // Wait for a line to arrive
-  Serial5.setTimeout(TIMEOUT_SEAPHOX_MS);
-
-  // GPS
-  // Settings D6 LOW powers up the GPS module
   // The GPS module on the AssetTracker is connected to Serial1 and D6
   Serial1.begin(9600);
   pinMode(D6, OUTPUT);
-  digitalWrite(D6, LOW);
+  digitalWrite(D6, LOW); // Settings D6 LOW powers up the GPS module
+  startFix = millis(); // keep track of how long it takes to get first fix
+  gettingFix = true;
+
+  // SeapHOx serial; wait TIMEOUT_SEAPHOX_MS for a line to arrive
+  Serial5.setTimeout(TIMEOUT_SEAPHOX_MS);
 }
 
-// Give SeapHOx arbitrary wake character then "glast" command. Then listen for response.
+
 void loop() {
-
-  // Clean out any residual junk in buffer and restart serial port
-  Serial5.end();
-  delay(1000);
-  Serial5.begin(115200);
-  delay(500);
-
-  // SeapHOx Sampling
-  // Get data in file after current file pointer
-  Serial5.println("glast");
-
-  // Read response
-  s = Serial5.readString();			// read response
-  String s2 = s.replace("Error.txt f_read error: FR_OK\r\n", "");
-  const char* s_args = s2.c_str();
-  char* each_var = strtok(strdup(s_args), "\t");
-  Serial.println(s_args);
-
-  // Parse response
-  // parseSeapHOx(each_var); // UNCOMMENT FOR REAL TEST
-
-  // GPS Sampling
   while (Serial1.available() > 0) {
-    // Serial.write(Serial1.read());
     if (gps.encode(Serial1.read())) {
-      displayInfo();
+      parseGPS();
     }
   }
 
-  // Publish to cloud
-  publishSeapHOxFake(); // REMOVE "FAKE" FOR REAL TEST
+  //////////////////////////////////////////////////////////////////////////////
+  // Enter state machine
+  switch(state) {
 
-  // Calculate sleep time
-  int nextSampleMin = 5; // sample at 5 past the hour
-  int currentHour = Time.hour();
-  int currentSecond = Time.now()%86400; // in UTC
+  //////////////////////////////////////////////////////////////////////////////
+  /*** GPS_WAIT_STATE ***/
+  /*** Start here. Continue spinning in loop() while in GPS_WAIT_STATE to see
+  if we get a GPS fix. If we get a fix or time exceeds MAX_TIME_FOR_GPS_FIX_MS, that'll
+  kick us into PUBLISH_STATE. ***/
+  case GPS_WAIT_STATE:
+    Particle.connect();
+    if (gps.location.isValid()) {
+    // Got a GPS fix
+      latitude = gps.location.lat();
+      longitude = gps.location.lng();
+      state = PUBLISH_STATE;
+      break;
+    }
+    if (millis() - stateTime >= MAX_TIME_FOR_GPS_FIX_MS) {
+      Serial.println("failed to get GPS fix");
+      latitude = -999;
+      longitude = -999;
+      state = PUBLISH_STATE;
+      break;
+    }
+  break;
 
-  // Calculate seconds since midnight of next sample
-  int nextSampleSec = (currentHour+1)*60*60+nextSampleMin*60; // sample at this time
-  int secondsToSleep = nextSampleSec - currentSecond; // UNCOMMENT for deployment
+  //////////////////////////////////////////////////////////////////////////////
+  /*** PUBLISH_STATE ***/
+  /*** Get here from PUBLISH_STATE. Ensure that we're connected to Particle Cloud.
+  If so, poll SeapHOx, parse response, and send that and GPS info to cloud then
+  go to SLEEP_STATE
 
-  // int secondsToSleep = 180; // COMMENT OUT during deployment
-  Serial.printf("Sleep for %d seconds\n", secondsToSleep);
-  // System.sleep(SLEEP_MODE_DEEP, secondsToSleep);
+  If not connected, still poll SeapHOx and print out response over USB serial then go to SLEEP_STATE.
+  ***/
+  case PUBLISH_STATE:
 
-  delay(180000); // COMMENT OUT during deployment
+    // Poll SeapHOx:
+    // Clean out any residual junk in buffer and restart serial port
+    Serial5.end();
+    delay(1000);
+    Serial5.begin(115200);
+    delay(500);
+
+    // Get data in file after current file pointer
+    Serial5.println("glast");
+
+    // Read SeapHOx response
+    s = Serial5.readString();			// read response
+    String s2 = s.replace("Error.txt f_read error: FR_OK\r\n", "");
+    const char* s_args = s2.c_str();
+    char* each_var = strtok(strdup(s_args), "\t");
+
+    // Parse SeapHOx response
+    parseSeapHOx(each_var); 
+
+    // Put Electron, SeapHOx, and GPS data into data buffer and print to screen
+    char data[100];
+    float cellVoltage = batteryMonitor.getVCell();
+    float stateOfCharge = batteryMonitor.getSoC();
+    snprintf(data, sizeof(data), "%.02f,%.02f,%f,%f,%.3f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.02f,%.02f",
+              cellVoltage, stateOfCharge, latitude, longitude,
+              SeapHOx_Cell.Board_Date, SeapHOx_Cell.Board_Time,
+              SeapHOx_Cell.Main_Batt_Volt, SeapHOx_Cell.V_FET_INT,
+              SeapHOx_Cell.V_FET_EXT, SeapHOx_Cell.V_Pressure,
+              SeapHOx_Cell.pHINT, SeapHOx_Cell.O2uM,
+              SeapHOx_Cell.SBE37_Temp, SeapHOx_Cell.SBE37_Salinity
+            );
+    Serial.println(data);
+
+    // If connected, publish data buffer
+    if (Particle.connected()) {
+      Particle.publish(eventName, data, 60, PRIVATE);
+
+      // Wait for the publish to go out by spinning here till enough time has elapsed
+      stateTime = millis();
+      while (millis() - stateTime < TIME_AFTER_PUBLISH_MS) {
+        delay(10);
+      }
+      state = SLEEP_STATE;
+    }
+
+    // If not connected after certain amount of time, go to sleep to save battery
+    else {
+
+      // Took too long to publish, just go to sleep
+      if (millis() - stateTime >= MAX_TIME_TO_PUBLISH_MS) {
+        state = SLEEP_STATE;
+      }
+    }
+  break;
+
+  //////////////////////////////////////////////////////////////////////////////
+  /*** SLEEP_STATE ***/
+  /*** Get here from PUBLISH_STATE and go to GPS_WAIT_STATE (if code makes it that far)
+  or SLEEP_MODE_DEEP after calculating a wakeup time based off of the current time from the cloud.
+  ***/
+  case SLEEP_STATE:
+    Serial.println("going to sleep");
+    delay(500);
+
+    // Test delay vs. sleep
+    // delay(180000);
+
+    // Calculate sleep time
+  	int nextSampleMin = 5; // sample at 5 past the hour
+  	int currentHour = Time.hour();
+  	int currentSecond = Time.now()%86400; // in UTC
+
+  	// Calculate seconds since midnight of next sample
+  	int nextSampleSec = (currentHour+1)*60*60+nextSampleMin*60; // sample at this time
+   	int secondsToSleep = nextSampleSec - currentSecond;
+  	Serial.printf("Sleep for %d seconds\n", secondsToSleep);
+   	System.sleep(SLEEP_MODE_DEEP, secondsToSleep);
+
+    // It'll only make it here if the sleep call doesn't work for some reason
+    stateTime = millis();
+    state = GPS_WAIT_STATE;
+  break;
+
+  }
 }
+
+void parseGPS()
+{
+  if (millis() - lastSerial >= SERIAL_PERIOD_MS) {
+    lastSerial = millis();
+
+    char buf[128];
+    if (gps.location.isValid()) {
+      snprintf(buf, sizeof(buf), "%f,%f", gps.location.lat(), gps.location.lng());
+      if (gettingFix) {
+        gettingFix = false;
+        unsigned long elapsed = millis() - startFix;
+        Serial.printlnf("%lu milliseconds to get GPS fix", elapsed);
+      }
+    }
+    else {
+      strcpy(buf, "no location");
+      if (!gettingFix) {
+        gettingFix = true;
+        startFix = millis();
+      }
+    }
+    Serial.println(buf);
+  }
+}
+
 
 void parseSeapHOx(char* new_var){
  	// Parse SeapHOx response
@@ -154,130 +268,30 @@ void parseSeapHOx(char* new_var){
  	if (parsed[0][8] == '#') {
  		SeapHOx_Cell.Board_Date 			= parsed[1];
  		SeapHOx_Cell.Board_Time 			= parsed[2];
- 		SeapHOx_Cell.Main_Batt_Volt   = strtof(parsed[3], NULL);  //3 yes V
+ 		SeapHOx_Cell.Main_Batt_Volt   = strtof(parsed[3], NULL);
  		SeapHOx_Cell.V_Therm          = strtof(parsed[4], NULL);
  		SeapHOx_Cell.V_FET_INT        = strtof(parsed[5], NULL);
  		SeapHOx_Cell.V_FET_EXT        = strtof(parsed[6], NULL);
- 		SeapHOx_Cell.Durafet_Temp     = strtof(parsed[9], NULL); //9 yes C
+ 		SeapHOx_Cell.Durafet_Temp     = strtof(parsed[9], NULL);
  		SeapHOx_Cell.V_Pressure       = strtof(parsed[10], NULL);
- 		SeapHOx_Cell.pHINT            = strtof(parsed[11], NULL); //11 yes pH
- 		SeapHOx_Cell.pHEXT            = strtof(parsed[12], NULL); //12 yes pH
- 		SeapHOx_Cell.O2               = strtof(parsed[17], NULL); //17 yes [µM]
+ 		SeapHOx_Cell.pHINT            = strtof(parsed[11], NULL);
+ 		SeapHOx_Cell.pHEXT            = strtof(parsed[12], NULL);
+ 		SeapHOx_Cell.O2uM             = strtof(parsed[17], NULL);
  		SeapHOx_Cell.O2_Saturation    = strtof(parsed[18], NULL);
  		SeapHOx_Cell.Optode_Temp      = strtof(parsed[19], NULL);
- 		SeapHOx_Cell.SBE37_Temp       = strtof(parsed[20], NULL); //27 yes C
- 		SeapHOx_Cell.SBE37_Cond       = strtof(parsed[21], NULL); //28 yes cm^^-1
- 		SeapHOx_Cell.SBE37_Salinity   = strtof(parsed[22], NULL); //29 yes s
+ 		SeapHOx_Cell.SBE37_Temp       = strtof(parsed[20], NULL);
+ 		SeapHOx_Cell.SBE37_Cond       = strtof(parsed[21], NULL);
+ 		SeapHOx_Cell.SBE37_Salinity   = strtof(parsed[22], NULL);
 
     Serial.printf("\nParsed SeapHOX \n Date-time %s-%s\n Main_Batt_Volt %.5f\n V_Therm %.5f\n V_FET_INT %.5f\n V_FET_EXT %.5f\n Durafet_Temp %2.5f\n V_Pressure %.5f\n pHINT %.5f\n pHEXT %.5f\n O2 %.5f\n O2_Saturation  %.5f\n Optode_Temp %.5f\n SBE37_Temp %.5f\n SBE37_Cond %.5f\n SBE37_Salinity %.5f\n",
-    							SeapHOx_Cell.Board_Date,
-    							SeapHOx_Cell.Board_Time,
-    							SeapHOx_Cell.Main_Batt_Volt ,
-                  SeapHOx_Cell.V_Therm ,
-                  SeapHOx_Cell.V_FET_INT,
-                  SeapHOx_Cell.V_FET_EXT ,
-                  SeapHOx_Cell.Durafet_Temp ,
-                  SeapHOx_Cell.V_Pressure,
-                  SeapHOx_Cell.pHINT,
-                  SeapHOx_Cell.pHEXT,
-                  SeapHOx_Cell.O2,
-                  SeapHOx_Cell.O2_Saturation,
-                  SeapHOx_Cell.Optode_Temp,
-                  SeapHOx_Cell.SBE37_Temp,
-                  SeapHOx_Cell.SBE37_Cond,
-                  SeapHOx_Cell.SBE37_Salinity
+    							SeapHOx_Cell.Board_Date, SeapHOx_Cell.Board_Time,
+    							SeapHOx_Cell.Main_Batt_Volt, SeapHOx_Cell.V_Therm ,
+                  SeapHOx_Cell.V_FET_INT, SeapHOx_Cell.V_FET_EXT ,
+                  SeapHOx_Cell.Durafet_Temp, SeapHOx_Cell.V_Pressure,
+                  SeapHOx_Cell.pHINT, SeapHOx_Cell.pHEXT,
+                  SeapHOx_Cell.O2uM, SeapHOx_Cell.O2_Saturation,
+                  SeapHOx_Cell.Optode_Temp, SeapHOx_Cell.SBE37_Temp,
+                  SeapHOx_Cell.SBE37_Cond, SeapHOx_Cell.SBE37_Salinity
                 );
   }
 }
-
-void displayInfo()
-{
-	if (millis() - lastSerial >= SERIAL_PERIOD_MS) {
-		lastSerial = millis();
-
-		char buf[128];
-		if (gps.location.isValid()) {
-			snprintf(buf, sizeof(buf), "%f,%f,%f", gps.location.lat(), gps.location.lng(), gps.altitude.meters());
-			if (gettingFix) {
-				gettingFix = false;
-				unsigned long elapsed = millis() - startFix;
-				Serial.printlnf("%lu milliseconds to get GPS fix", elapsed);
-			}
-		}
-		else {
-			strcpy(buf, "no location");
-			if (!gettingFix) {
-				gettingFix = true;
-				startFix = millis();
-			}
-		}
-		Serial.println(buf);
-
-		// if (Particle.connected()) {
-		// 	if (millis() - lastPublish >= PUBLISH_PERIOD) {
-		// 		lastPublish = millis();
-		// 		Particle.publish("gps", buf);
-		// 	}
-		// }
-	}
-
-}
-
-void publishSeapHOxFake(void){
- 	char data[100];
- 	int count_NC = 0;
- 	float cellVoltage = batteryMonitor.getVCell();
- 	float stateOfCharge = batteryMonitor.getSoC();
-
- 	snprintf(data, sizeof(data), "Sampling! It's been %d seconds since 1-Jan-1970.", Time.now());
-
- 	Serial.println(data); // did we make it into here?
-
- 	// if (Particle.connected() == false) {
- 	// 	Serial.println("Wasn't Connected");
- 	// 	Particle.connect();
- 	// }
-
- // 	Particle.publish(eventName, data, 60, PRIVATE);
- 	delay(4000); // wait for publish to go out before sleeping
-}
- // void publishSeapHOx(void){
- // 	char data[100];
- // 	int count_NC = 0;
- // 	float cellVoltage = batteryMonitor.getVCell();
- // 	float stateOfCharge = batteryMonitor.getSoC();
- //
- // 	// Should perhaps put this and Serial.print(data) into if statement (like parseSeapHOx, above)
- // 	snprintf(data, sizeof(data), "%s,%s,%.3f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.02f,%.02f,%f,%f",
- // 					 SeapHOx_Cell.Board_Date,
- // 					 SeapHOx_Cell.Board_Time,
- // 					 SeapHOx_Cell.Main_Batt_Volt ,
- // 					 SeapHOx_Cell.V_FET_INT,
- // 					 SeapHOx_Cell.V_FET_EXT ,
- // 					 SeapHOx_Cell.V_Pressure,
- // 					 SeapHOx_Cell.pHINT,
- // 					 SeapHOx_Cell.O2,
- // 					 SeapHOx_Cell.SBE37_Temp,
- // 					 SeapHOx_Cell.SBE37_Salinity,
- // 				 	 cellVoltage,
- // 					 stateOfCharge,
-  //          gps.location.lat(),
-  //          gps.location.lng()
-  //         );
- //
- // 	Serial.println(data); // did we make it into here?
- //
- // 	// if (Particle.connected() == false) {
- // 	// 	Serial.println("Wasn't Connected");
- // 	// 	Particle.connect();
- // 	// }
- //
- // 	Particle.publish(eventName, data, 60, PRIVATE);
- // 	delay(4000); // wait for publish to go out before sleeping
- // }
-
- // TODO: add GPS (?)
-
- // TODO: add logic to force connection attempt to stop after designated time
-
- // TODO: Add particle function for TS
